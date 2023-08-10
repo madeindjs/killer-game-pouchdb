9e43e0a43f6ec2e8fe38cf7fd788d43d8df3fea2
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-08-10 22:11:37 +0200

    rename form

--- src/app/games/[id]/page.tsx
+++ src/app/games/[id]/page.tsx
@@ -1,10 +1,10 @@
-import Game from "../../components/Game";
+import GameForm from "../../components/GameForm";
 
 export default function GameIdPage({ params: { id } }: { params: { id: string } }) {
   return (
     <div>
       <h1>game: {id}</h1>
-      <Game id={id} />
+      <GameForm id={id} />
     </div>
   );
 }
