1923e95ad5b2a88a4754fedc15c6644add408e83
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-08-10 22:10:21 +0200

    set loading state

--- src/app/components/Game.tsx
+++ src/app/components/Game.tsx
@@ -7,6 +7,7 @@ import { Game } from "../../models/game";
 export default function Game({ id }: { id: string }) {
   const db = usePouchDB(id);
 
+  const [loading, setLoading] = useState(false);
   const [game, setGame] = useState<Game>({ name: "untitled" });
 
   async function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
@@ -16,7 +17,6 @@ export default function Game({ id }: { id: string }) {
 
   async function saveGame() {
     const existingGame = await db.get<Game>("game").catch(() => undefined);
-
     const isDirty = game.name !== existingGame?.name;
 
     if (isDirty) db.put({ _id: "game", ...existingGame, name: game.name }, {});
@@ -24,7 +24,10 @@ export default function Game({ id }: { id: string }) {
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    saveGame().catch(console.error);
+    setLoading(true);
+    saveGame()
+      .catch(console.error)
+      .finally(() => setLoading(false));
   }
 
   function listenPouchDBChanges() {
@@ -35,19 +38,21 @@ export default function Game({ id }: { id: string }) {
 
   useEffect(() => {
     listenPouchDBChanges();
+    setLoading(true);
     db.get<Game>("game")
       .then((existingGame) => setGame({ ...game, name: existingGame.name }))
-      .catch();
+      .catch()
+      .finally(() => setLoading(false));
   }, [id]);
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} aria-busy={loading}>
         <div>
-          <label htmlFor="">name</label>
-          <input type="text" value={game?.name} onChange={handleNameChange} />
+          <label htmlFor="gameName">name</label>
+          <input type="text" id="gameName" value={game?.name} onChange={handleNameChange} readOnly={loading} />
         </div>
-        <input type="submit" />
+        <input type="submit" disabled={loading} />
       </form>
     </div>
   );
